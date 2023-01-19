3f0f2c3cefa87833aee3976bc7de4ce9271e1154
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-01-19 12:16:06 -0300

    refactory add a const with how many iteration are there

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,8 +1,10 @@
 package iteration
 
+const iterationMany = 5
+
 func Repeat(character string) string {
 	characters := ""
-	for i := 0; i < 5; i++ {
+	for i := 0; i < iterationMany; i++ {
 		characters += character
 	}
 	return characters
